7bb9f6ebf1ad92538a40c9727a41c597eceb62a8
harrisonharva <harrisonharva@gmail.com>
2017-07-22 17:17:35 +0530

    Added configuration related changes for setup

--- server/server.js
+++ server/server.js
@@ -1,13 +1,4 @@
-var env = process.env.NODE_ENV || 'development';
-console.log(`Environment ************************* ${env}`);
-if(env == 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/todoapp'
-} else {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/todoappTest'
-}
-//var config = require('./config/config');
+const config = require('./config/config');
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
