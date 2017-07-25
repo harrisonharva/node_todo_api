26f9bed614d67a431f587fcf501df1e1d9746f13
harrisonharva <harrisonharva@gmail.com>
2017-07-26 01:37:41 +0530

    Add test case for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -272,6 +272,28 @@ describe('POST /users/login', () => {
     });
 });
 
+describe('DELETE /users/me/token', () => {
+    it("It should remove auth token on logout", (done) => {
+        request(app)
+        .delete("/users/me/token")
+        .set('x-auth', users[0].tokens[0].token)
+        .send()
+        .expect(200)
+        .expect((res) => {
+            expect(res.headers['x-auth']).toNotExist();
+        })
+        .end((err, res) => {
+            if(err) {
+                return done(err);
+            }
+            User.findById(users[0]._id).then((user) => {
+                expect(user.tokens.length).toBe(0);
+                done();
+            }).catch((err) => done(err));
+        })
+    });
+});
+
 describe('POST /users', () => {
     it('It should create a user if everything is valid', (done) => {
         var email = "example@example.com";
